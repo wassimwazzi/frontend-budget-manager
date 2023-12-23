fd45d6094c278d7cc8c7c1287c7584ee2e3c0af0
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2023-12-23 13:45:41 -0500

    status fade out

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,4 +1,23 @@
 import LoadingSpinner from "./LoadingSpinner";
+import { Fade } from "react-bootstrap";
+import React, { useEffect } from "react";
+
+const FadeOut = ({ children, delay = 10_000 }) => {
+    const [show, setShow] = React.useState(true);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setShow(false);
+        }, delay);
+        return () => clearTimeout(timeout);
+    }, [delay]);
+
+    return (
+        <Fade in={show}>
+            <div>{children}</div>
+        </Fade>
+    );
+};
 
 const Status = ({ loading, successMessage, errorMessage }) => {
     if (loading) {
@@ -6,16 +25,20 @@ const Status = ({ loading, successMessage, errorMessage }) => {
     }
     if (successMessage) {
         return (
-            <div className='alert alert-success mx-3' role='alert'>
-                {successMessage}
-            </div>
+            <FadeOut>
+                <div className='alert alert-success mx-3' role='alert'>
+                    {successMessage}
+                </div>
+            </FadeOut>
         )
     }
     if (errorMessage) {
         return (
-            <div className='alert alert-danger mx-3' role='alert'>
-                {errorMessage}
-            </div>
+            <FadeOut>
+                <div className='alert alert-danger mx-3' role='alert'>
+                    {errorMessage}
+                </div>
+            </FadeOut>
         )
     }
     return <></>
