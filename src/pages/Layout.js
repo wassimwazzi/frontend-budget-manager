a80ff3f534acd7bf0ab38103b8255feddd9c88ae
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2023-12-24 16:38:44 -0500

    Navbar style

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,56 +1,76 @@
-import React from 'react'
-import { Outlet, Link, Navigate } from 'react-router-dom'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import React from 'react';
+import { Outlet, Link, Navigate, useLocation } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Layout = () => {
+  const location = useLocation();
+
   // Function to handle logout
   const handleLogout = () => {
     // Implement your logout logic here
     // For example, remove the authToken from localStorage
-    localStorage.removeItem('authToken')
+    localStorage.removeItem('authToken');
     // Redirect user back to the login page
-    window.location.href = '/login'
-  }
+    window.location.href = '/login';
+  };
 
-  const isAuthenticated = localStorage.getItem('authToken') !== null
+  const isAuthenticated = localStorage.getItem('authToken') !== null;
 
   if (!isAuthenticated) {
-    return <Navigate to='/login' />
+    return <Navigate to="/login" />;
   }
 
   return (
     <Container>
-      <Navbar bg='light' expand='lg'>
-        <Navbar.Brand as={Link} to='/'>
+      <Navbar expand="lg" variant="light" className="mb-4 border-bottom">
+        <Navbar.Brand as={Link} to="/" className="font-weight-bold text-primary">
           Dashboard
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
-            <Nav.Link as={Link} to='/transactions'>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link
+              as={Link}
+              to="/transactions"
+              className={location.pathname === '/transactions' ? 'active text-primary' : ''}
+            >
               Transactions
             </Nav.Link>
-            <Nav.Link as={Link} to='/categories'>
+            <Nav.Link
+              as={Link}
+              to="/categories"
+              className={location.pathname === '/categories' ? 'active text-primary' : ''}
+            >
               Categories
             </Nav.Link>
-            <Nav.Link as={Link} to='/budgets'>
+            <Nav.Link
+              as={Link}
+              to="/budgets"
+              className={location.pathname === '/budgets' ? 'active text-primary' : ''}
+            >
               Budgets
             </Nav.Link>
-            <Nav.Link as={Link} to='/uploads'>
+            <Nav.Link
+              as={Link}
+              to="/uploads"
+              className={location.pathname === '/uploads' ? 'active text-primary' : ''}
+            >
               File Uploads
             </Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            <Nav.Link onClick={handleLogout} className="text-muted">
+              Logout
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
 
-      <Container style={{ margin: '20px' }}>
+      <Container fluid className="p-4">
         <Outlet />
       </Container>
     </Container>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
