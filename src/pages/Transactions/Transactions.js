fefa0eafefea189914eba05d22eaa972beb67205
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2024-06-01 04:44:59 -0400

    add searching back

--- src/pages/Transactions/Transactions.js
+++ src/pages/Transactions/Transactions.js
@@ -13,6 +13,7 @@ import TransactionsDisplay from './TransactionsDisplay'
 import FloatingIcon from '../../components/FloatingIcon'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import TableNavigator from '../../components/table/TableNavigator'
+import SearchTable from '../../components/table/SearchTable'
 
 
 
@@ -29,6 +30,8 @@ const Transactions = () => {
   const [deleteErrorMessage, setDeleteErrorMessage] = useState(null)
   const [showModal, setShowModal] = useState(false)
   const [nextPrev, setNextPrev] = useState([null, null])
+  const [searchParams, setSearchParams] = useState({ page: 1 })
+  const [sortParams, setSortParams] = useState({ sort: 'date', order: 'desc' })
 
   const fetchData = useCallback((params) => {
     api
@@ -42,23 +45,6 @@ const Transactions = () => {
         console.error('Error fetching data:', error.response)
       })
   }, [])
-
-  const handleExportToCsv = (params) => {
-    api
-      .get('/api/exports/transactions/', { params })
-      .then(response => {
-        const url = window.URL.createObjectURL(new Blob([response.data]))
-        const link = document.createElement('a')
-        link.href = url
-        link.setAttribute('download', 'transactions.csv')
-        document.body.appendChild(link)
-        link.click()
-        link.remove()
-      })
-      .catch(error => {
-        console.error('Error exporting data:', error)
-      })
-  }
 
   useEffect(() => {
     api
@@ -78,7 +64,7 @@ const Transactions = () => {
         console.error('Error fetching currency data:', error.response)
       })
 
-    fetchData({ page: 1, sort: 'date', order: 'desc' })
+    fetchData({ ...searchParams, ...sortParams })
   }, [fetchData])
 
 
@@ -92,6 +78,8 @@ const Transactions = () => {
     'currency',
     'actions'
   ]
+
+  const searchColumns = ['date', 'code', 'description', 'category', 'amount', 'currency']
 
   const handleEdit = transactionId => {
     setEditTransactionId(transactionId)
@@ -155,6 +143,49 @@ const Transactions = () => {
     setShowModal(false)
   }
 
+  const handlePageChange = page => {
+    setSearchParams({ ...searchParams, page })
+    fetchData({ ...searchParams, page })
+  }
+
+  const getParamsHelper = (initialParams, newParams) => {
+    const params = { ...initialParams }
+    newParams.forEach(search => {
+      if (params.filter && params.filter_value) {
+        params.filter.push(search.column);
+        params.filter_value.push(search.term);
+      }
+      else {
+        params.filter = [search.column];
+        params.filter_value = [search.term];
+      }
+    })
+    return params
+  }
+  const handleSearch = (searchTerms) => {
+    const params = getParamsHelper({ page: 1 }, searchTerms)
+    setSearchParams(params)
+    fetchData({ ...params, ...sortParams })
+  }
+
+  const handleExportToCsv = (params) => {
+    params = getParamsHelper({}, params)
+    api
+      .get('/api/exports/transactions/', { params })
+      .then(response => {
+        const url = window.URL.createObjectURL(new Blob([response.data]))
+        const link = document.createElement('a')
+        link.href = url
+        link.setAttribute('download', 'transactions.csv')
+        document.body.appendChild(link)
+        link.click()
+        link.remove()
+      })
+      .catch(error => {
+        console.error('Error exporting data:', error)
+      })
+  }
+
   return (
     <>
       <FloatingIcon onClick={handleAdd}>
@@ -191,8 +222,9 @@ const Transactions = () => {
         exportData={handleExportToCsv}
         searchColumns={['date', 'code', 'description', 'category', 'amount', 'currency']}
       /> */}
+      <SearchTable columns={searchColumns} exportData={handleExportToCsv} onSearch={handleSearch} />
       <TransactionsDisplay transactions={transactions} handleDelete={handleDelete} handleEdit={handleEdit} />
-      <TableNavigator initialPage={1} totalPages={totalPages} onPageChange={(page) => fetchData({ page, sort: 'date', order: 'desc' })} />
+      <TableNavigator initialPage={1} totalPages={totalPages} onPageChange={handlePageChange} />
     </>
   )
 }
