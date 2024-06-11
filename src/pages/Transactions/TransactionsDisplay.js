107d3c500ff36d9ef70ce93e6bf258788a4707e0
wassimwazzi <wassim.wazzi@mail.mcgill.ca>
2024-06-11 11:39:26 -0400

    dashboard and transaction improvements

--- src/pages/Transactions/TransactionsDisplay.js
+++ src/pages/Transactions/TransactionsDisplay.js
@@ -6,6 +6,7 @@ import { DeleteButton } from '../../components/ActionButtons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoneyBill1Wave, faMoneyBillTrendUp, faFileImport, faLightbulb } from '@fortawesome/free-solid-svg-icons'
 import PlaidTransactionCard from './PlaidTransaction';
+import styled from 'styled-components';
 
 function CustomToggle({ children, eventKey }) {
 
@@ -19,11 +20,50 @@ function CustomToggle({ children, eventKey }) {
     );
 }
 
-const IncomeOrExpenseIcon = ({ income }) => {
-    if (income) {
-        return <FontAwesomeIcon icon={faMoneyBillTrendUp} size='2x' color='green' />
+const IconWrapper = styled.div`
+  .icon {
+    font-size: 1.5rem; /* Default size for small screens */
+  }
+
+  @media (min-width: 600px) {
+    .icon {
+      font-size: 2rem; /* Medium screens */
     }
-    return <FontAwesomeIcon icon={faMoneyBill1Wave} size='2x' />
+  }
+
+  @media (min-width: 900px) {
+    .icon {
+      font-size: 2.5rem; /* Large screens */
+    }
+  }
+
+  @media (min-width: 1200px) {
+    .icon {
+      font-size: 3rem; /* Extra large screens */
+    }
+  }
+`;
+
+const dateDisplay = (date) => {
+    const dateObj = new Date(date);
+    const thisYear = new Date().getFullYear();
+    if (dateObj.getFullYear() === thisYear) {
+        return dateObj.toLocaleDateString('default', { month: 'short', day: 'numeric' });
+    }
+    return dateObj.toLocaleDateString('default', { year: 'numeric', month: 'short', day: 'numeric' });
+}
+
+
+const IncomeOrExpenseIcon = ({ income }) => {
+    // if (income) {
+    //     return <FontAwesomeIcon icon={faMoneyBillTrendUp} size='xs' color='green' />
+    // }
+    // return <FontAwesomeIcon icon={faMoneyBill1Wave} size='s' />
+    return (
+        <IconWrapper>
+            <FontAwesomeIcon icon={income ? faMoneyBillTrendUp : faMoneyBill1Wave} className='icon' />
+        </IconWrapper>
+    )
 }
 
 const InferredCategoryPill = () => {
@@ -52,6 +92,15 @@ const TransactionElementStyle = {
     whiteSpace: 'nowrap'
 }
 
+const InlineElement = ({ display, val }) => {
+    return (
+        <div>
+            <h6 style={{ display: 'inline-block' }}>{`${display} `}</h6>
+            <p style={{ display: 'inline-block', marginLeft: '2px' }}>{val}</p>
+        </div>
+    )
+}
+
 const TransactionsDisplay = ({ transactions, handleDelete, handleEdit }) => {
     const TransactionItem = ({ transaction, id }) => (
         <div className='p-3' style={{ overflow: 'scroll' }}>
@@ -61,34 +110,31 @@ const TransactionsDisplay = ({ transactions, handleDelete, handleEdit }) => {
                         <div style={{ flex: 1 }}>
                             <IncomeOrExpenseIcon income={transaction.category.income} />
                         </div>
-                        <h5 style={TransactionElementStyle}>{transaction.date}</h5>
-                        <h5 style={TransactionElementStyle}>{transaction.category.category}</h5>
                         <p style={TransactionElementStyle}>{transaction.code}</p>
-                        <div style={TransactionElementStyle} className='d-flex'>
-                            <h5 >{transaction.amount}</h5>
-                            <p style={{ marginLeft: '0.5rem' }}>{transaction.currency}</p>
+                        <h6 style={TransactionElementStyle}>{transaction.category.category}</h6>
+                        <div style={TransactionElementStyle}>
+                            <h6 >${transaction.amount}</h6>
+                            <p>{dateDisplay(transaction.date)}</p>
                         </div>
                     </div>
                 </Item>
                 <Accordion.Collapse eventKey={id}>
-                    <Item>
-                        <div style={{ borderTop: '1px solid #dee2e6' }}>
-                            <div className='d-flex justify-content-around' style={{ marginTop: '1rem' }}>
-                                {transaction.description && <p>Description: {transaction.description}</p>}
-                                {transaction.inferred_category && <div><InferredCategoryPill /></div>}
-                                {transaction.file && <div><FontAwesomeIcon icon={faFileImport} size='1x' /> {transaction.file.file}</div>}
-                            </div>
+                    <div className='py-2 px-1' style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)' }}>
+                        <div className='d-flex justify-content-around' style={{ marginTop: '1rem' }}>
+                            {transaction.description && <InlineElement display={'Description:'} val={transaction.description} />}
+                            <InlineElement display={'Category:'} val={transaction.category.category} />
+                            {transaction.inferred_category && <div><InferredCategoryPill /></div>}
+                            {transaction.file && <div><FontAwesomeIcon icon={faFileImport} size='1x' /> {transaction.file.file}</div>}
                         </div>
                         {transaction.plaid_transaction && <PlaidTransactionCard transaction={transaction.plaid_transaction} />}
                         <div className='d-flex justify-content-center'>
                             <button onClick={() => handleEdit(transaction.id)} className='btn btn-secondary'>Edit</button>
                             <DeleteButton handleDelete={() => handleDelete(transaction.id)} warningMessage={'Are you sure you want to delete this transaction?'} />
                         </div>
-
-                    </Item>
+                    </div>
                 </Accordion.Collapse>
             </CustomToggle>
-        </div >
+        </div>
     )
 
     return (
